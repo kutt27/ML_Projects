109a2fe534e622aee5f979d6ac2a9553cd17176c
kutt27 <satheesanamal6@gmail.com>
2025-11-17 12:57:47 +0530

    updated

---
+++ GenAI/Level-2-Application/Interview Question Creator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Local Package",
+    version= '0.0.0',
+    author="Temp",
+    author_email="temp@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
